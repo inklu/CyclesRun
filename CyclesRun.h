06adc86e8f387dbbb704855376046713a957eeb0
Igor Klyushnyov <50979964+inklu@users.noreply.github.com>
2019-06-11 12:21:26 +0700

    Create CyclesRun.h

---
+++ CyclesRun.h
@@ -0,0 +1,25 @@
+//// CyclesRun
+class CyclesRun{
+  public:
+    typedef void (*pInvoke)(); //type description of the pointer to void func()
+    struct cycle { 
+      uint16_t cycleMls;
+      pInvoke *funcs;//=nullptr;
+    };
+    CyclesRun(){};
+    CyclesRun(const cycle _c[]);
+    void addCycle(const uint16_t _mls, const pInvoke _pi[]=nullptr);
+    void run(uint64_t _mls=0);
+    void start(){ isRun=true; };
+    void pause(){ isRun=false; };
+  protected:
+    virtual void onEveryMls(uint64_t mls=0){};
+    virtual void onEveryCycle(const uint16_t cycleMls, const uint64_t mls=0){};
+  private:
+    struct cyclePrv:cycle {
+      uint16_t mls;
+    } *cycles;// = nullptr;
+    size_t cycleSize;
+    static uint64_t mls;
+    bool isRun=false;
+};
